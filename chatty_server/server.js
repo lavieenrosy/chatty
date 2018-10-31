527600144097afb3f9a6277d53ef9875f99b77cb
Rosy Lee <rosyhlee@gmail.com>
2018-10-31 21:48:05 +0000

    used websockets to establish server communication with multiple clients for chat feature

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const uuidv1 = require('uuid/v1');
+const WebSocket = require('ws');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -13,18 +14,29 @@ const server = express()
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
+const clients = [];
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  clients.push(ws);
 
   ws.on('message', function incoming(data) {
-    const message = JSON.parse(data);
-    const uuid = uuidv1();
-    console.log("UUID: ", uuid);
+    const incomingData = JSON.parse(data);
+    const id = uuidv1();
+    const username = incomingData.currentUser;
+    const content = incomingData.messages;
+    const message = {id, username, content}
     console.log("Message: ", message);
+
+    clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN && client != ws) {
+        client.send(JSON.stringify(message));
+      }
+    });
+
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
